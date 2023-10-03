aacbbdd1773d423204dc08cf5590304ea34df1cb
KHALILUR RAHMAN <89288313+khalil518@users.noreply.github.com>
2023-10-04 00:11:26 +0600

    commit

---
+++ draw line above in image/draw line above the image.py
@@ -0,0 +1,30 @@
+
+# importing cv2
+import cv2
+
+# Reading an image in default mode
+image = cv2.imread("image/car1.jpg")
+image1=cv2.resize(image,(500,500))
+
+# Window name in which image is displayed
+window_name = 'Image'
+
+# Start coordinate, here (0, 0)
+# represents the top left corner of image
+start_point = (10, 10)
+
+# End coordinate, here (250, 250)
+# represents the bottom right corner of image
+end_point = (300, 300)
+
+# Green color in BGR
+color = (0, 0, 250)
+
+# Line thickness of 9 px
+thickness = 5
+image2 = cv2.line(image1, start_point, end_point, color, thickness)
+
+# Displaying the image
+cv2.imshow(window_name, image2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

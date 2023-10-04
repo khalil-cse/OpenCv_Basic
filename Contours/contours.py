1f32ebe29e3d35d5d000ce0a10eb832c9e955c1e
KHALILUR RAHMAN <89288313+khalil518@users.noreply.github.com>
2023-10-04 13:05:42 +0600

    commit

---
+++ Contours/contours.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('image/rice.jpg')
+image=cv2.resize(image,(500,500))
+
+# Grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Find Canny edges
+edged = cv2.Canny(gray, 30, 250)
+
+# Finding Contours
+# Use a copy of the image e.g. edged.copy()
+# since findContours alters the image
+contours, hierarchy = cv2.findContours(edged,
+	cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+cv2.imshow('Canny Edges After Contouring', edged)
+
+print("Number of Contours found = " + str(len(contours)))
+
+# Draw all contours
+# -1 signifies drawing all contours
+cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+
+cv2.imshow('Contours', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
